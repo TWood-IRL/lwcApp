328c58ca9d38dfe04ddcc0dcfe821a9aaf255a8e
Thomas Woodhouse <thomaswoodhouse93@gmail.com>
2019-09-17 16:52:30 +0100

    testing synthetic shadow

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,6 @@
+import '@lwc/synthetic-shadow';
+
 import { buildCustomElementConstructor } from 'lwc';
+
 import MyApp from 'my/app';
 customElements.define('my-app', buildCustomElementConstructor(MyApp));
